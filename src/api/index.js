847c68829f346b679e9805a2bc3b031b511e32c6
taimoorkhan1122 <khant8k83@gmail.com>
2021-07-21 21:36:29 +0500

    api url added

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import moment from "moment";
 
-const url = "http://localhost:5000/api/test/jobs";
+const url = "https://zippia-server.herokuapp.com/api/test/jobs";
 
 const getDate = (postingDate) => {
   const date = postingDate.replace("T", " ");
